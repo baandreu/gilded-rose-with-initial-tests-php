bc91f0ccd3556df3a34c74329ec61237dfe606ac
Ricard Osorio <celtric@gmail.com>
2016-10-17 19:03:43 +0200

    extract constant

--- src/GildedRose/Items/MutableItem.php
+++ src/GildedRose/Items/MutableItem.php
@@ -6,6 +6,8 @@ use GildedRose\Item;
 
 abstract class MutableItem implements NewItem
 {
+    const MAX_QUALITY = 50;
+
     /** @var Item */
     protected $legacyItem;
 
@@ -19,7 +21,7 @@ abstract class MutableItem implements NewItem
 
     protected function increaseQuality()
     {
-        if ($this->legacyItem->quality < 50) {
+        if ($this->legacyItem->quality < self::MAX_QUALITY) {
             $this->legacyItem->quality = $this->legacyItem->quality + 1;
         }
     }
